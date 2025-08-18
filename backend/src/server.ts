c0165b3d3f290e71beb59ef8b69479d1716ae87d
Isabela Oliveira <isabela676@gmail.com>
2025-08-18 18:54:03 -0300

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -35,7 +35,7 @@ const PORT = Number(process.env.PORT) || 3333
 app
   .listen({ port: PORT, host: '0.0.0.0' })
   .then(() => {
-    console.log(`🚀 Server running at http://localhost:${PORT}`)
+    console.log(`🚀 Server running at http://0.0.0.0:${PORT}`)
   })
   .catch(err => {
     console.error('❌ Error starting server:', err)
